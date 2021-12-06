4c4786ec8f13404a3d7bb0f3745cc1c3e4316348
mefuru <mehul.mandania@gmail.com>
2021-12-06 18:59:03 +0100

    Add mobile page, update breakpoints, make responsive

--- pages/grants.tsx
+++ pages/grants.tsx
@@ -5,7 +5,6 @@ import { MobileFooter } from "components/MobileFooter";
 import Navbar from "components/NavBar";
 import Link from "next/link";
 import React, { useState } from "react";
-import * as Icon from "react-feather";
 import { Menu } from "react-feather";
 
 const IndexPage = () => {
@@ -37,24 +36,11 @@ const IndexPage = () => {
           leaveTo="opacity-0"
         >
           <div>
-            <header className="w-full bg-primary">
-              <Link href="https://launch.popcorn.network/" passHref>
-                <a target="_window">
-                  <div className="w-full h-14 bg-yellow-500 shadow-md flex justify-center cursor-pointer hover:bg-yellow-400">
-                    <div className="flex flex-row items-center mx-auto">
-                      <p className="text-white text-2xl font-bold">
-                        Token Launch Auction
-                      </p>
-                      <Icon.ArrowRightCircle className="ml-2 w-7 h-7 text-white" />
-                    </div>
-                  </div>
-                </a>
-              </Link>
-              <nav className="w-10/12 mx-auto pt-12 pb-3 border-b border-primaryLight flex flex-row items-center justify-between">
+            <header className="w-full bg-white border-b border-gray-300">
+              <nav className="w-10/12 mx-auto pt-4 pb-3  flex flex-row items-center justify-between">
                 <div>
                   <Link href="/" passHref>
                     <a>
-                      {/*TODO The logo is slightly blurred even though its copied straight from figma*/}
                       <img
                         src="/images/logo.png"
                         alt="Logo"
@@ -66,11 +52,14 @@ const IndexPage = () => {
                 <Menu onClick={(e) => toggleMenu(true)} />
               </nav>
             </header>
-            <section className="w-10/12 mx-auto mb-40">
+            <section
+              className="w-10/12 mx-auto "
+              style={{ marginBottom: "200px" }}
+            >
               <h2 className="font-bold text-3xl mb-8 text-center mt-12">
                 Get Grant
               </h2>
-              <p className="text-lg font-landing text-gray-500 text-center">
+              <p className="text-lg font-light font-landing text-gray-500 text-center ">
                 Potential beneficiaries of the Popcorn Foundation must be
                 nominated by the Popcorn Foundation or by POP token holders
                 through a multi-step beneficiary nomination process.
@@ -92,6 +81,128 @@ const IndexPage = () => {
                 </a>
               </p>
             </section>
+            <section className="mb-40" style={{ height: "400px" }}>
+              <div className="relative">
+                <img
+                  className="absolute z-0 top-0"
+                  src="images/grantmobile/mobilegrantleft.svg"
+                />
+
+                <div className="absolute z-20" style={{ top: "-64px" }}>
+                  <img
+                    className="mx-auto"
+                    src="images/grantmobile/env.png"
+                    style={{ marginBottom: "64px" }}
+                  />
+                  <h1
+                    className="font-semibold text-3xl font-gray-900 text-center"
+                    style={{ marginBottom: "16px" }}
+                  >
+                    Environment
+                  </h1>
+                  <div className="sm:w-10/12 mx-auto">
+                    <p
+                      className="text-base font-light font-landing text-gray-500 text-center"
+                      style={{ marginRight: "24px", marginLeft: "24px" }}
+                    >
+                      We are committed to supporting activities that strengthen
+                      the conservation and protection of natural resources,
+                      advance ecological sustainability, and foster harmony
+                      between communities and the environment. We empower
+                      communities to access grants and other resources, embrace
+                      local solutions, and spur innovation in an effort to
+                      address the causes and reduce the effects of climate
+                      change and environmental degradation.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section className="mb-40">
+              <img
+                className="mx-auto"
+                src="images/grantmobile/foss.png"
+                style={{ marginBottom: "64px" }}
+              />
+              <h1
+                className="font-semibold text-3xl font-gray-900 text-center"
+                style={{ marginBottom: "16px" }}
+              >
+                Free and Open Source Software
+              </h1>
+              <div className="sm:w-10/12 mx-auto">
+                <p
+                  className="text-base font-light font-landing text-gray-500 text-center"
+                  style={{ marginRight: "24px", marginLeft: "24px" }}
+                >
+                  We support the development of free and open source software
+                  that promotes fair and equitable access, creating opportunity
+                  in underserved and global communities alike.
+                </p>
+              </div>
+            </section>
+
+            <section className="" style={{ marginBottom: "264px" }}>
+              <img
+                className="mx-auto"
+                src="images/grantmobile/ed.png"
+                style={{ marginBottom: "64px" }}
+              />
+              <h1
+                className="font-semibold text-3xl font-gray-900 text-center"
+                style={{ marginBottom: "16px" }}
+              >
+                Education
+              </h1>
+              <div className="sm:w-10/12 mx-auto">
+                <p
+                  className="text-base font-light font-landing text-gray-500 text-center"
+                  style={{ marginRight: "24px", marginLeft: "24px" }}
+                >
+                  Our goal is to strengthen the capacity of communities to
+                  support basic education and literacy, reduce gender disparity
+                  in education, and increase adult literacy. We support
+                  education for all children and literacy for children and
+                  adults.
+                </p>
+              </div>
+            </section>
+
+            <section className="mb-40" style={{ height: "560px" }}>
+              <div className="relative">
+                <img
+                  className="absolute z-0 top-0 right-0"
+                  src="images/grantmobile/mobilegrantright.svg"
+                />
+
+                <div className="absolute  z-20" style={{ top: "-100px" }}>
+                  <img
+                    className="mx-auto mb-20"
+                    src="images/grantmobile/inequality.png"
+                  />
+                  <h1
+                    className="font-semibold text-3xl font-gray-900 text-center sm:mb-24"
+                    style={{ marginBottom: "16px" }}
+                  >
+                    Inequality
+                  </h1>
+                  <div className="sm:w-10/12 mx-auto">
+                    <p
+                      className="text-base font-light font-landing text-gray-500 text-center"
+                      style={{ marginRight: "24px", marginLeft: "24px" }}
+                    >
+                      Our goal is to help reduce inequality and enable a life
+                      lived in dignity and with equal rights for all. To reduce,
+                      inequality it is vital to overcome discrimination, shift
+                      mindsets toward a fairer distribution of resources, and
+                      ensure that everyone can participate in society, politics,
+                      and the economy.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+
             <MobileFooter />
           </div>
         </Transition>
@@ -116,7 +227,7 @@ const IndexPage = () => {
               <h2 className="w-11/12 font-bold text-6xl xl:text-7xl leading-snug mb-8">
                 Get Grant
               </h2>
-              <p className="text-2xl font-landing text-gray-500 w-[800px]">
+              <p className="text-2xl font-landing text-gray-500 w-[800px] font-light">
                 Potential beneficiaries of the Popcorn Foundation must be
                 nominated by the Popcorn Foundation or by POP token holders
                 through a multi-step beneficiary nomination process.
@@ -154,7 +265,7 @@ const IndexPage = () => {
               <h2 className="w-11/12 font-bold text-5xl xl:text-6xl leading-snug mb-4">
                 Environment
               </h2>
-              <p className="text-2xl font-landing text-gray-500 w-[620px]">
+              <p className="text-2xl font-landing text-gray-500 w-[620px] font-light">
                 We are committed to supporting activities that strengthen the
                 conservation and protection of natural resources, advance
                 ecological sustainability, and foster harmony between
@@ -166,14 +277,9 @@ const IndexPage = () => {
             </div>
             <div className="w-6/12 relative flex flex-row justify-end">
               <img
-                src="/images/environment.png"
+                src="/images/enivronment2.png"
                 alt="tree"
                 className="z-10"
-              ></img>
-              <img
-                src="/images/lightYellowPentagon.svg"
-                alt="tree"
-                className="z-0 absolute -ml-16 bottom-0 left-0 -mb-32"
               ></img>
             </div>
           </div>
@@ -191,7 +297,7 @@ const IndexPage = () => {
               <h2 className="font-bold text-5xl xl:text-6xl leading-snug mb-4 2xl:w-9/12">
                 Free and Open Source Software
               </h2>
-              <p className="text-2xl font-landing text-gray-500 2xl:w-10/12 w-[620px]">
+              <p className="text-2xl font-landing text-gray-500 2xl:w-10/12 w-[620px]  font-light">
                 We support the development of free and open source software that
                 promotes fair and equitable access, creating opportunity in
                 underserved and global communities alike.
@@ -212,7 +318,7 @@ const IndexPage = () => {
               <h2 className="w-11/12 font-bold text-5xl xl:text-6xl leading-snug mb-4">
                 Education
               </h2>
-              <p className="text-2xl font-landing text-gray-500 w-[620px]">
+              <p className="text-2xl font-landing text-gray-500 w-[620px]  font-light">
                 Our goal is to strengthen the capacity of communities to support
                 basic education and literacy, reduce gender disparity in
                 education, and increase adult literacy. We support education for
@@ -225,21 +331,16 @@ const IndexPage = () => {
           </div>
         </section>
 
-        <section className="bg-impact-pattern flex-shrink-0 flex-grow-0 w-full h-full impact-background relative mt-20">
+        <section className="bg-impact-pattern flex-shrink-0 flex-grow-0 w-full h-full impact-background relative mt-20 mb-100">
           <div className="w-10/12 mx-auto flex flex-row justify-between items-center">
             <div className="w-7/12 2xl:w-8/12 relative flex flex-row justify-start">
-              <img src="/images/equalrights.png" alt="tree" className="z-10" />
-              <img
-                src="/images/secondPentagon.svg"
-                alt="tree"
-                className="z-0 absolute top-0 right-48 -mt-32"
-              />
+              <img src="/images/inequality2.png" alt="tree" className="z-10" />
             </div>
             <div className="w-5/12 2xl:w-4/12">
               <h2 className="font-bold text-5xl xl:text-6xl leading-snug mb-4 2xl:w-9/12">
                 Inequality
               </h2>
-              <p className="text-2xl font-landing text-gray-500 2xl:w-10/12 z-10 relative w-[620px]">
+              <p className="text-2xl font-landing text-gray-500 2xl:w-10/12 z-10 relative w-[620px]  font-light">
                 Our goal is to help reduce inequality and enable a life lived in
                 dignity and with equal rights for all. To reduce inequality, it
                 is vital to overcome discrimination, shift mindsets toward a
