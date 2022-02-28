507f145be9e65beacdc7174dee3928e86de503bf
RedVeil <leon.niesler@gmail.com>
2022-02-28 17:33:55 +0100

    updated styling in mobile menu

--- components/MobileExpandableMenu.tsx
+++ components/MobileExpandableMenu.tsx
@@ -1,7 +1,8 @@
-import { XIcon } from "@heroicons/react/solid";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { Facebook, GitHub, Twitter } from "react-feather";
+import { XIcon } from "@heroicons/react/solid";
 
 interface MobileExpandableMenuProps {
   toggleMenuVisible: (boolean) => void;
@@ -10,100 +11,88 @@ interface MobileExpandableMenuProps {
 export const MobileExpandableMenu: React.FC<MobileExpandableMenuProps> = ({
   toggleMenuVisible,
 }) => {
+  const router = useRouter();
+
   return (
-    <div className="absolute z-10 w-full overflow-y-auto ">
-      <div className="relative w-full h-screen mobile-menu-bg transition-opacity duration-1000 ease-in-out">
-        <div className="w-10/12 mx-auto ">
-          <div className="grid justify-items-stretch">
+    <div className="fixed h-full overflow-y-scroll z-10 nav-width mobile-menu-bg">
+      <div className="relative w-full min-h-screen h-full transition-opacity duration-1000 ease-in-out ">
+        <div className="w-screen px-6 mx-auto nav-animation">
+        <div className="grid justify-items-stretch">
             <XIcon
               onClick={() => toggleMenuVisible(false)}
               className="mt-8 mb-2 text-white justify-self-end"
               style={{ width: 24, height: 24 }}
             />
           </div>
-          <div className="w-full bg-white " style={{ height: 0.32 }}></div>
-          <Link href="/about" passHref>
-            <a>
-              <h1
-                className="ml-3 text-4xl font-bold text-white hover:text-gray-700 font-landing"
-                style={{ marginTop: 18, marginBottom: 18 }}
-              >
-                About Us
-              </h1>
-            </a>
-          </Link>
-          <div
-            className="w-full bg-white opacity-50"
-            style={{ height: 0.72 }}
-          ></div>
+          <div className="pt-5 flex flex-col divide-y divide-white divide-opacity-50">
+            <Link href="/about" passHref>
+              <a>
+                <h1
+                  className={`${
+                    router.pathname === "/about"
+                      ? "text-gray-700"
+                      : "text-white"
+                  } ml-3 text-4xl font-bold hover:text-gray-700 font-landing py-5 border-t border-white border-opacity-50`}
+                >
+                  About Us
+                </h1>
+              </a>
+            </Link>
+            <Link href="/grants" passHref>
+              <a>
+                <h1
+                  className={`${
+                    router.pathname === "/grants"
+                      ? "text-gray-700"
+                      : "text-white"
+                  } ml-3 text-4xl font-bold hover:text-gray-700 font-landing py-5`}
+                >
+                  Get Grant
+                </h1>
+              </a>
+            </Link>
+            <Link href="/ideas" passHref>
+              <a>
+                <h1
+                  className={`${
+                    router.pathname === "/ideas"
+                      ? "text-gray-700"
+                      : "text-white"
+                  } ml-3 text-4xl font-bold hover:text-gray-700 font-landing py-5`}
+                >
+                  Ideas
+                </h1>
+              </a>
+            </Link>
+            <Link href="/popcorndao" passHref>
+              <a>
+                <h1
+                  className={`${
+                    router.pathname === "/popcorndao"
+                      ? "text-gray-700"
+                      : "text-white"
+                  } ml-3 text-4xl font-bold hover:text-gray-700 font-landing py-5`}
+                >
+                  Popcorn DAO
+                </h1>
+              </a>
+            </Link>
+            <Link href="/partners" passHref>
+              <a>
+                <h1
+                  className={`${
+                    router.pathname === "/partners"
+                      ? "text-gray-700"
+                      : "text-white"
+                  } ml-3 text-4xl font-bold hover:text-gray-700 font-landing py-5`}
+                >
+                  Partners
+                </h1>
+              </a>
+            </Link>
+          </div>
 
-          <Link href="/grants" passHref>
-            <a>
-              <h1
-                className="ml-3 text-4xl font-bold text-white hover:text-gray-700 font-landing"
-                style={{ marginTop: 18, marginBottom: 18 }}
-              >
-                Get Grant
-              </h1>
-            </a>
-          </Link>
-          <div
-            className="w-full bg-white opacity-50"
-            style={{ height: 0.72 }}
-          ></div>
-
-          <Link href="/ideas" passHref>
-            <a>
-              <h1
-                className="ml-3 text-4xl font-bold text-white hover:text-gray-700 font-landing"
-                style={{ marginTop: 18, marginBottom: 18 }}
-              >
-                Ideas
-              </h1>
-            </a>
-          </Link>
-          <div
-            className="w-full bg-white opacity-50"
-            style={{ height: 0.72 }}
-          ></div>
-          <Link href="/popcorndao" passHref>
-            <a>
-              <h1
-                className="ml-3 text-4xl font-bold text-white hover:text-gray-700 font-landing"
-                style={{ marginTop: 18, marginBottom: 18 }}
-              >
-                Popcorn DAO
-              </h1>
-            </a>
-          </Link>
-          <div
-            className="w-full bg-white opacity-50"
-            style={{ height: 0.72 }}
-          ></div>
-
-          <Link href="/partners" passHref>
-            <a>
-              <h1
-                className="ml-3 text-4xl font-bold text-white hover:text-gray-700 font-landing"
-                style={{ marginTop: 18, marginBottom: 18 }}
-              >
-                Partners
-              </h1>
-            </a>
-          </Link>
-          <div
-            className="w-full bg-white opacity-50"
-            style={{ height: 0.72 }}
-          ></div>
-
-          <div
-            className="w-full bg-white opacity-50"
-            style={{ height: 0.72 }}
-          ></div>
-          <div
-            className="w-full grid grid-cols-4 my-5 mx-auto justify-items-center"
-            style={{ marginTop: 18, marginBottom: 18 }}
-          >
+          <div className="grid w-full grid-cols-4 mx-auto py-5 justify-items-center border-t border-b border-white border-opacity-50 mt-6 mb-5">
             <div>
               <Link href="https://github.com/popcorndao" passHref>
                 <GitHub
@@ -133,59 +122,53 @@ export const MobileExpandableMenu: React.FC<MobileExpandableMenuProps> = ({
                 <img
                   src="/images/discord-white-thick.svg"
                   alt="discord"
-                  className=" text-white cursor-pointer discord"
+                  className="text-white cursor-pointer discord"
                   style={{ width: 33, height: 33 }}
                 ></img>
               </Link>
             </div>
           </div>
-          <div className="w-full bg-white" style={{ height: 0.72 }}></div>
-          <div className="flex flex-row justify-evenly py-6">
-            <div className="flex flex-col space-y-3 w-1/2">
-              <p className="font-bold uppercase text-base font-landing text-white">
-                Site
-              </p>
+          <div className="flex flex-row py-6 justify-evenly">
+            <div className="flex flex-col w-1/2 space-y-3">
+              <p className="text-base text-white uppercase font-bold">Site</p>
               <Link href="/" passHref>
-                <a className="text-xl text-white font-thin">Home</a>
+                <a className="text-xl text-white font-medium">Home</a>
               </Link>
               <Link href="https://medium.com/popcorndao" passHref>
-                <a className="text-xl text-white font-thin" target="_window">
+                <a className="text-xl text-white font-medium" target="_window">
                   Blog
                 </a>
               </Link>
             </div>
-            <div className="ml-20 flex flex-col space-y-3 w-1/2">
-              <p className="font-bold uppercase text-base font-landing text-white">
+            <div className="flex flex-col w-1/2 ml-20 space-y-3">
+              <p className="text-base text-white uppercase font-bold">
                 Connect
               </p>
               <Link href="https://twitter.com/Popcorn_DAO" passHref>
-                <a className="text-xl text-white font-thin" target="_window">
+                <a className="text-xl text-white font-medium" target="_window">
                   Twitter
                 </a>
               </Link>
               <Link href="https://discord.gg/w9zeRTSZsq" passHref>
-                <a className="text-xl text-white font-thin" target="_window">
+                <a className="text-xl text-white font-medium" target="_window">
                   Discord
                 </a>
               </Link>
               <Link href="https://github.com/popcorndao" passHref>
-                <a className="text-xl text-white font-thin" target="_window">
+                <a className="text-xl text-white font-medium" target="_window">
                   Github
                 </a>
               </Link>
             </div>
           </div>
-          <div
-            className="flex flex-col space-y-3 mt-10"
-            style={{ paddingBottom: 100 }}
-          >
-            {/* <p className="font-bold uppercase text-base font-landing text-white">
+          {/* <div className="flex flex-col mt-10 space-y-3" style={{paddingBottom: 100}}>
+            <p className="text-base font-bold text-white uppercase font-landing">
               Documentation
             </p>
             <Link href="/" passHref>
               <a className="text-xl text-white text-semibold">Gitbook</a>
-            </Link> */}
-          </div>
+            </Link> 
+          </div> */}
         </div>
       </div>
     </div>
