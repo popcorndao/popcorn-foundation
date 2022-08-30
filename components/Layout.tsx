895d3bfb04bffdc923472b00a9b89b307e1cd993
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-08-30 11:32:42 +0100

    chore: create reuseable layout compontnt

---
+++ components/Layout.tsx
@@ -0,0 +1,25 @@
+import Navbar from "components/Navigation/NavBar";
+import Footer from "./Footer";
+
+const Layout = (props: { children: React.ReactNode }) => {
+  return (
+    <div className="font-khTeka px-6 lg:px-8 mx-auto">
+      <div className="flex-col w-full h-full">
+        <header className="w-full h-[112px]">
+          <Navbar />
+        </header>
+        <main className="mt-4 md:mt-[48px]">
+          {props.children}
+        </main>
+        <div className="lg:mt-12">
+          <div className="py-6 hidden md:block">
+            <img src="/images/footer-background.svg" alt="" className="rounded-lg w-full object-cover" />
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
