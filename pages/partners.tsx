1cb20a7ef1611c8992c97c8a8c82b46ae5b356bc
Adenuga Oluwatunmise <adenuga@popcorn.network>
2022-09-01 12:02:34 +0100

    chore: update partners image referencing

--- pages/partners.tsx
+++ pages/partners.tsx
@@ -8,18 +8,18 @@ export default function Partners(): JSX.Element {
       <div className="col-span-12 md:col-span-8 md:col-start-4 ">
         <h6 className="text-3xl lg:text-6xl text-black font-normal leading-[100%] mb-10 md:mb-20">Our Partners</h6>
         <SliderContainer slidesToShow={7}>
-          <img src="/images/Partners/banklessdao.png" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
-          <img src="/images/Partners/ohm.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
-          <img src="/images/Partners/giveth.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
-          <img src="/images/Partners/newform.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
-          <img src="/images/Partners/kenetic.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
-          <img src="/images/Partners/bigbrainholding.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
-          <img src="/images/Partners/impossiblefinance.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
-          <img src="/images/Partners/cryptofoundersnetwork.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
-          <img src="/images/Partners/hesticholdings.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
-          <img src="/images/Partners/jumpcap.png" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
-          <img src="/images/Partners/theLAO.png" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
-          <img src="/images/Partners/xbto.png" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
+          <img src="/images/partners/banklessdao.png" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
+          <img src="/images/partners/ohm.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
+          <img src="/images/partners/giveth.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
+          <img src="/images/partners/newform.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
+          <img src="/images/partners/kenetic.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
+          <img src="/images/partners/bigbrainholding.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
+          <img src="/images/partners/impossiblefinance.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
+          <img src="/images/partners/cryptofoundersnetwork.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
+          <img src="/images/partners/hesticholdings.svg" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
+          <img src="/images/partners/jumpcap.png" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
+          <img src="/images/partners/theLAO.png" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
+          <img src="/images/partners/xbto.png" alt="" className="px-2 md:px-5 w-10 h-10 object-contain" />
         </SliderContainer>
       </div>
       <div className="grid grid-cols-12 mt-10 md:mt-20">
