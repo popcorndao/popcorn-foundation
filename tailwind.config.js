ca7c962c7c8e37898b8a71787ae794438a34eac2
RedVeil <leon.niesler@gmail.com>
2022-09-13 13:14:51 +0200

    got rid of webpack

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ module.exports = {
   purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./container/**/*.{js,ts,jsx,tsx}",
   ],
   mode: "jit",
   darkMode: false, // or 'media' or 'class'
